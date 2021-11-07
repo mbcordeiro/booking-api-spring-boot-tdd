a0f580ee73564e473c5dfb6c3f3ab6d7041e7d23
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-11-07 19:48:38 -0300

    Booking service impl

---
+++ src/main/java/com/matheuscordeiro/bookingapi/service/BookingService.java
@@ -0,0 +1,10 @@
+package com.matheuscordeiro.bookingapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+    public int dayCalculator(String name) {
+        return 10;
+    }
+}
--- src/test/java/com/matheuscordeiro/bookingapi/BookingServiceTest.java
+++ src/test/java/com/matheuscordeiro/bookingapi/BookingServiceTest.java
@@ -1,22 +1,22 @@
 package com.matheuscordeiro.bookingapi;
 
+import com.matheuscordeiro.bookingapi.service.BookingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
     @Autowired
-    BookingService  bookingService;
+    BookingService bookingService;
 
     @Test
     public void bookingServiceDaysCalculator() {
         var name = "Name";
         var days = bookingService.dayCalculator(name);
         assertEquals(days, 10);
-
     }
 }
